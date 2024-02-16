32b8c9bb165e2072ca2b753ae048d27c48a16f79
Santhosh <santhoshkumar@dsignzmedia.in>
2024-02-16 13:12:25 +0530

    cdn link fix

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -62,6 +62,7 @@
 <!-- Select2 CDN -->
 {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.1/css/select2.min.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.1/js/select2.min.js"></script> --}}
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <script>
     $(document).ready(function() {
